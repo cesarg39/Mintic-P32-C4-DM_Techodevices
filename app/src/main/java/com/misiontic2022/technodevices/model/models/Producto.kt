b1d19e9a631c0913a5769e335f14f851235e261b
AndresSArias <andres31321@gmail.com>
2021-11-29 18:34:26 -0500

    Creación modelo producto

---
+++ app/src/main/java/com/misiontic2022/technodevices/model/models/Producto.kt
@@ -0,0 +1,8 @@
+package com.misiontic2022.technodevices.model.models
+
+data class Producto(
+    var imagen: String = "",
+    var titulo: String = "",
+    var precio: String = "",
+    var descripcion: String = ""
+)
