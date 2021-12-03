ff698a4ab1834f469587a6ea2e19b2fe1d0b2a94
Andabero5 <adbeltranr@correo.udistrital.edu.co>
2021-12-02 19:43:53 -0500

    maps

--- app/src/main/java/com/misiontic2022/technodevices/view/ui/fragments/MapsLocationFragment.kt
+++ app/src/main/java/com/misiontic2022/technodevices/view/ui/fragments/MapsLocationFragment.kt
@@ -1,37 +1,116 @@
 package com.misiontic2022.technodevices.view.ui.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.misiontic2022.technodevices.R
 import com.misiontic2022.technodevices.databinding.FragmentMapsLocationBinding
+import com.misiontic2022.technodevices.view.ui.activities.MainActivity
 
-class MapsLocationFragment : Fragment(R.layout.fragment_maps_location), OnMapReadyCallback {
+class MapsLocationFragment : Fragment(R.layout.fragment_maps_location), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
     private lateinit var binding: FragmentMapsLocationBinding
 
     private lateinit var map: GoogleMap
-
+    companion object {
+        const val LOCATION_REQUEST_CODE = 0
+    }
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(requireContext(), "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        } else {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE)
+        }
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMapsLocationBinding.bind(view)
         createMapFragment()
 
+
     }
 
+
     private fun createMapFragment() {
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.fragmentMap) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        createMarker()
+        map.setOnMyLocationButtonClickListener(this)
+        map.setOnMyLocationClickListener(this)
+        enableMyLocation()
     }
 
+    private fun createMarker() {
+        val favoritePlace = LatLng(28.044195, -16.5363842)
+        map.addMarker(MarkerOptions().position(favoritePlace).title("Mi playa favorita!"))
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(favoritePlace, 18f),
+            4000,
+            null
+        )
+    }
 
+    private fun isPermissionsGranted() = ContextCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
 
+    private fun enableMyLocation() {
+        if (!::map.isInitialized) return
+        if (isPermissionsGranted()) {
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            LOCATION_REQUEST_CODE -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                map.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Para activar la localización ve a ajustes y acepta los permisos",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+        }
+    }
+    override fun onMyLocationButtonClick(): Boolean {
+        //Toast.makeText(requireContext(), "Boton pulsado", Toast.LENGTH_SHORT).show()
+        return false
+    }
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(requireContext(), "Estás en ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
+    }
 }
+
+
+
